eecf9850787735ddc2b588c09a2bb3e1950962be
Crt Ahlin <crt.ahlin@gmail.com>
2013-01-05 19:01:10 +0100

    debugging

--- R/PlotTests.R
+++ R/PlotTests.R
@@ -161,6 +161,9 @@ plotTests <- function (data, figureParameters, graphsDir = getwd(),
   
   # calculate buffer for y axis size (account for drawing lines between groups)
   
+  # debugging
+  podatki <<- data
+  browser()
   
   # generate plot with number of days of tests on the x axis
   # and appropriate y axis
