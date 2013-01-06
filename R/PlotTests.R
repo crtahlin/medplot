66780491fa6a85abca83ed099eb04a17be4c65b0
Crt Ahlin <crt.ahlin@gmail.com>
2013-01-06 10:35:59 +0100

    bugfix

--- R/PlotTests.R
+++ R/PlotTests.R
@@ -423,11 +423,12 @@ plotTests <- function (data, figureParameters, graphsDir = getwd(),
     mtext(line=2, text=TEST.RESULT.LEVELS[i], side=3,
           las=1,  cex=.75, padj=1, font=2,
           at=max(daysofTests)/4+i*5-5, col=DOT.COLORS[i])
+    
     points(x=max(daysofTests)/4+i*5-5,
            y=2,
            col=DOT.COLORS[i],
            cex=DOT.SIZES[i],
-           pch=16
+           pch=16)
   }
   
   # add label of graph
