8150ad384063507bf8a80facc8e46b9b58ca360b
Crt Ahlin <crt.ahlin@gmail.com>
2013-02-01 08:44:01 +0100

    added some comments to the Symptoms code

--- inst/shinyapp_symptoms/server.R
+++ inst/shinyapp_symptoms/server.R
@@ -1,25 +1,28 @@
+# This is the server part of the shiny script to plot
+# graph for displaying presence of symptoms. 
+
 # load library for generation interactive web pages
 library(shiny)
 # load library for generating graph scales
 library(scales)
 
 
-# Define server logic required to plot various variables against mpg
+# Main function
 shinyServer(function(input, output) {
-
+  
   # subset the data with the selected symptoms
   data <- reactive( function() {
     symptomsData[symptomsData$variable %in% input$selectedSymptoms,]
   }
-    )
+  )
   
   # plot the graph, but only for selected symptoms
   output$plot <- reactivePlot( function() {
     print(
       ggplot(data(), aes(x = Date, y = Patient, size = value, colour = variable)) +
-      geom_point(shape = 1) + theme_bw() + scale_size(range= c(1,20)) +
+        geom_point(shape = 1) + theme_bw() + scale_size(range= c(1,20)) +
         scale_x_date(breaks = date_breaks("1 week"),
-                          labels = date_format("%d %b")) +
+                     labels = date_format("%d %b")) +
         scale_x_date(minor_breaks = date_breaks("1 day"))
       
     )
@@ -28,5 +31,3 @@ shinyServer(function(input, output) {
   
 })
 
-#ms[ms$variable %in% c("Symptom1","Symptom2"),]
-#x.sub2 <- subset(x.df, y > 2 & V2 > 0.4, select = c(V1, V4))
--- inst/shinyapp_symptoms/ui.R
+++ inst/shinyapp_symptoms/ui.R
@@ -1,23 +1,29 @@
+# This is the GUI part of the shiny script to plot
+# graph for displaying presence of symptoms. 
+
 library(shiny)
 
+# Prepare the list of possible symptoms
 reportedSymptoms <- unlist(levels(symptomsData[,"variable"]))
 
-# Define UI for miles per gallon application
+# Define UI for displaying presence of symptoms
 shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel("Patients and their symptoms"),
   
+  # Define the selection panel
   sidebarPanel(wellPanel(
     checkboxGroupInput(inputId="selectedSymptoms",
                        label=h5("Symptoms:"),
                        choices=reportedSymptoms)
   )
-    ),
+  ),
   
+  # Define the output panel
   mainPanel(
     plotOutput("plot")
-    
-    )
-))
+  )
+)
+)
 
