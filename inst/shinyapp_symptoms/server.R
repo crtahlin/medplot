4d435cf24e5eaf01485bb3ce2c4438cb74378fdb
Crt Ahlin <crt.ahlin@gmail.com>
2013-04-11 22:56:30 +0200

    symptoms are scaled by area.

--- inst/shinyapp_symptoms/server.R
+++ inst/shinyapp_symptoms/server.R
@@ -16,14 +16,22 @@ shinyServer(function(input, output) {
   
   # plot the graph, but only for selected symptoms
   output$plot <- renderPlot(function() {
+    # if no symbols are selected, do not plot
     if (dim(data())[1]>0) {
     print(
       ggplot(data(), aes(x = Date, y = Patient, size = value, colour = variable)) +
-        geom_point(shape = 1) + theme_bw() + scale_size(range= c(1,20)) +
+        geom_point(shape = 1) + theme_bw() +
+        # old scale - scale according to diameter
+        #scale_size(range= c(1,20)) +
+        # scale according to area
+        scale_size_area(breaks=c(1:10),minor_breaks=c(1:10),
+                        guide="legend",
+                        limits=c(1,10),
+                        max_size=10) +
         scale_x_date(breaks = date_breaks("1 week"),
-                     labels = date_format("%d %b")) +
-        scale_x_date(minor_breaks = date_breaks("1 day"))
-    
+                     labels = date_format("%d %b"),
+                     minor_breaks = date_breaks("1 day"))
+            
     )}
   })
   
