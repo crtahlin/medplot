9239857d5397b93c82a7782ddbf3947fd82e511d
Crt Ahlin <crt.ahlin@gmail.com>
2014-03-29 21:53:33 +0100

    Cleaned up code. All code code by Lara should be included now.

--- inst/shinyapp_symptoms2/server.R
+++ inst/shinyapp_symptoms2/server.R
@@ -26,66 +26,24 @@ library(rms)
 # library for logistic regression modeling
 library(logistf)
 
-# Global variables ---------------------------------------------------------------------
-# variables for melting data into ggplot compliant format for Timeline graph
-# obsolete?
-# meltBy <- c("PersonID", "Date", "Measurement")
-
 # TEMP for debuging
 # source("C:/Users/Crt Ahlin/Documents/Dropbox/medplot_package/R/TablePropWithSymptoms.r")
 
 # Main function -----------------------------------------------------------
 
 shinyServer(function(input, output, session) {
-  ### Import and prepare data ####
-  # load data from Excel
-  # TODO: are these functions below needed anymore?
-#   symptomsData <- reactive(function() { # returns the DATA sheet
-#     if (input$dataFileType=="Demo") {
-#       # load default data for Demo scenario
-#       # the location of template data file
-#       templateLocation <- paste0(path.package("medplot"),"/extdata/PlotSymptoms_shiny.xlsx")
-#       importSymptomsData(datafile=templateLocation,
-#                          format="Excel")
-#     } else { # load data for non-demo scenario
-#       if (!is.null(input$dataFile) && !(input$dataFileType=="Demo")) {
-#         importSymptomsData(datafile=input$dataFile$datapath,
-#                            format=input$dataFileType) }
-#     } 
-#   })
   
-#   symptomsPatients <- reactive(function() { # returns the PATIENTS sheet
-#     if (input$dataFileType=="Demo") {
-#       # load default data for Demo scenario
-#       # the location of template data file
-#       templateLocation <- paste0(path.package("medplot"),"/extdata/PlotSymptoms_shiny.xlsx")
-#       #if(is.null(input$dataFile)) return()
-#       importSymptomsPatients(datafile=templateLocation)    } #else {}
-#     # TODO: load patients for "Excel" scenario
-#   })
+  # FUNCTIONS ####
+  #how much space should be used for the graphical output of the Rcs estimates and others?  
+  NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
   
-  # subset the data with the selected symptoms - for 1st graph
-  # TODO: is this function still needed?
-#   data <- reactive( function() {
-#     data <- melt(symptomsData(), id.vars = meltBy)
-#     data[data$variable %in% input$selectedSymptoms,]
-#   })
-  
-#how much space should be used for the graphical output of the Rcs estimates and others?  
-NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
-
-  # list the subseted data in an output slot
-  output$data <- renderTable({
-    data <- dataExtended()
-    data$Date <- as.character(as.Date(data$Date, origin="1899-12-30"),format="%d.%m.%Y")
-    return(data)
-    # NOTE: if we want to render the table of data, we have to convert the dates into 
-    # characters, since renderTable seems to use xtable, which seems to not handle
-    # dates very well (http://stackoverflow.com/questions/8652674/r-xtable-and-dates)
+  # REACTIVE FUNCTIONS ####
+  # reactive - values of variable selected as measurement occasion variable ####
+  Measurement <- reactive({
+    dataFiltered()[,input$measurementVar]
   })
   
-  
-  # build extended data set for additional graphs
+  # reactive - data set with all the imported data ####
   dataExtended <- reactive( function() {
     observe(input$dataFile)
     # TODO: napiši kodo za scenarij, ko input$datafile ni null, ampak
@@ -106,7 +64,7 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                                      format="Excel")
       data <- join(x=symptoms, y=patients, by="PersonID", type="inner")
     }
-        
+    
     if (input$dataFileType=="TSV") {
       data <- importSymptomsData(datafile=input$dataFile$datapath,
                                  format="TSV")
@@ -114,6 +72,7 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
     return(data)
   })
   
+  # reactive - data set with data only for selected variables ####
   dataFiltered <- reactive(function(){
     data <- dataExtended()[ , 
                            c(input$patientIDVar,
@@ -127,70 +86,68 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
     return(data)
   })
   
-  ## Mainpanel dynamic output 
+  # reactive - returns the names of all column of imported data ####
+  dataVariableNames <- reactive(function(){
+    unlist(names(dataExtended()))
+  })
+  
+  # reactive - dataset with the positive/negative values for the selected symptoms ####
+  dataFiltered.yn=reactive({
+    #apply(symptomsData()[, -c(1:3)], 1, function(x) ifelse(x>input$threshold, 1, 0))
+    data=ifelse(dataFiltered()[, input$selectedSymptoms]>input$thresholdValue, 1, 0)
+    return(data)
+  })
+  
+  
+  # MAINPANEL ####
   # message for working with DEMO data
   output$message <- renderText(
     if (is.null(input$dataFile)) {paste("WORKING WITH DEMO DATA!")} else {
       if (dim(dataExtended())[1]==0){paste("Please select one or more symptoms.")}
     })
   
-  # debuging information
-  output$debug <- renderTable(dataFiltered.yn())
-  
+  # SIDEBAR ####
+  # GUI - selecting symptoms ####
   output$selectSymptoms <- renderUI({
-    # Sidebar for Excel and demo files ####
-    # TODO: Try to redo this part with switch() statement as
-    # it seems only the last evaluated block is returned
     if (!is.null(dataVariableNames())) {
-    #  data <- melt(symptomsData(), id.vars = meltBy)
-    #  symptoms <- unlist(levels(data[,"variable"]))
       checkboxGroupInput(inputId="selectedSymptoms",
                          label="Choose symptoms:", 
-                         #choices=symptoms
                          choices=dataVariableNames())}
   })
-  
-  dataVariableNames <- reactive(function(){
-    unlist(names(dataExtended()))
-  })
-  
+ 
+  # GUI - selecting Date variable ####
   output$selectDateVar <- renderUI({
-    #allSymptoms <- unlist(names(dataExtended()))
     selectInput(inputId="dateVar",
                 label="Choose date variable:", 
                 choices=dataVariableNames(),
                 selected="Date")
   })
   
+  # GUI - selecting grouping variable ####
   output$selectGroupingVar <- renderUI({
-    #allSymptoms <- unlist(names(dataExtended()))
     selectInput(inputId="groupingVar",
                 label="Choose grouping variable:", 
                 choices=dataVariableNames(),
                 selected="Sex")
   })
   
+  # GUI - selecting person ID variable ####
   output$selectPatientIDVar <- renderUI({
-    # allSymptoms <- unlist(names(dataExtended()))
     selectInput(inputId="patientIDVar",
                 label="Choose patient ID variable:", 
                 choices=dataVariableNames(),
                 selected="PersonID")
   })
   
+  # GUI - selecting measurements variable
   output$selectMeasurementVar <- renderUI({
-    #allSymptoms <- unlist(names(dataExtended()))
     selectInput(inputId="measurementVar",
                 label="Choose measurument occasion variable:", 
                 choices=dataVariableNames(),
                 selected="Measurement")
   })
   
-  ## Plots for different tabs ####
-  
-  # Timeline tab plots and output ####
-  # TODO: make data structure data() for TSV import
-  # plot the graph, but only for selected symptoms
+  # TAB - Timeline ####
   output$plotTimeline <- renderPlot(function() {
     data=dataFiltered()
     # observe({dataFiltered()})
@@ -201,22 +158,23 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                                personID=input$patientIDVar,
                                measurement=input$measurementVar,
                                symptoms=input$selectedSymptoms)
-    )#}
-    
+    )#}    
   })
   
-  # Proportions tab plots and output ####
-  # pyramid plot of proportions
+  # TAB - Proportions ####
+  # plot - pyramid plot of proportions ###
   output$plotPyramid <- renderPlot ({
     plotPropWithSymptoms(data=dataFiltered(),
                          grouping=input$groupingVar,
                          measurements=input$measurementVar,
                          symptomsNames=input$selectedSymptoms) #unlist(levels(data()[,"variable"]))
     # TODO: adapt the height of the figure based on the number of symptoms: does not work now>>>>
+    # CRT: it seems that heaight will also have to be set in plotOutput, otherwise the graph might not fit
+    # and window becomes scrollable (anoying)
   } ,height="auto")  #height=NumRows)
   
-  # Clustering tab plots and output ####
-  # selection of measurement occasions  
+  # TAB - Clustering ####
+  # ui - selection of measurement occasions  ###
   output$clusteringUI = renderUI({
     #levels of the measurement variable, save as third variable in the dataset symptomsData
     # TODO: make selection of levels dependent on Sidebar, not fixed to "Measurement"
@@ -227,7 +185,7 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                 choices=myLevels, selected=myLevels[1])
   })
   
-  # dendrogram plot on the Clustering tab
+  # plot - dendrogram plot on the Clustering tab ###
   output$plotClusterDendrogram=renderPlot({
     plotClusterDendrogram(data=dataFiltered(),
                           variableName=input$measurementVar,
@@ -235,7 +193,7 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                           selectedSymptoms=input$selectedSymptoms)
   })
   
-  # heatmap plot on the Clustering tab
+  # plot - heatmap plot on the Clustering tab ###
   output$plotClusterHeatmap=renderPlot({
     plotClusterHeatmap(data=dataExtended(),
                        #TODO: make dependent on selection
@@ -244,35 +202,16 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                        selectedSymptoms=input$selectedSymptoms) 
   })
   
-  output$symptomsData <- renderTable((dataExtended()))
-  
-  ### TODO: organize, make independent of column order
-  # TODO: rename into dataExtended.yn() ?; comment on data structure
-  #dataset with the positive/negative values for the selected symptoms
-  dataFiltered.yn=reactive({
-    #apply(symptomsData()[, -c(1:3)], 1, function(x) ifelse(x>input$threshold, 1, 0))
-    data=ifelse(dataFiltered()[, input$selectedSymptoms]>input$thresholdValue, 1, 0)
-    return(data)
-  })
-  
-  #saving Measurment in a reactive object - useful?
-  Measurement=reactive({
-    dataFiltered()[,input$measurementVar]
-  })
-  
+ 
+  # TAB - Distributions of symptoms ####
+  # ui - select measurement occasion ###
   output$proportionUI = renderUI({
-    
-    #levels of the measurement variable, save as third variable in the dataset symptomsData
-    # TODO: reference to measurementVar instead of 3rd column
-    my.levels=levels(as.factor(dataFiltered()[,input$measurementVar]))
-    
-    #select the measurement
     selectInput(inputId="measurementSelectedProportion",
-                label="Select the measurment (time)", 
-                choices=my.levels, selected=my.levels[1])
+                label="Select the measurement (time)", 
+                choices=Measurement(), selected=Measurement()[1])
   })
   
-  
+  # plot - proportions ###
   output$plotProportion=renderPlot({
     plotDistribution(data=dataFiltered.yn(),
                      selectedSymptoms=input$selectedSymptoms,
@@ -280,7 +219,7 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                      measurements=Measurement())
   })
   
-  
+  # plot - boxplots ###
   output$plotBoxplot=renderPlot({
     plotDistributionBoxplot(data=dataFiltered(),
                             data.yn=dataFiltered.yn(),
@@ -289,30 +228,27 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                             measurements=Measurement(),
                             posOnly=input$posOnly,
                             threshold=input$thresholdValue)
-    
-  })
-  
-  output$plotCI <- renderPlot({
-    
-    plotCI(data.yn=dataFiltered.yn(),
-            measurements=Measurement(),
-            selectedSymptoms=input$selectedSymptoms,
-            selectedProportion=input$measurementSelectedProportion)
-    
-    
     })
   
-
-  ########## user interface to select a numerical variable to associate with the presence of symptom
+  # plot - CI ###
+  output$plotCI <- renderPlot({
+    plotCI(data.yn=dataFiltered.yn(),
+           measurements=Measurement(),
+           selectedSymptoms=input$selectedSymptoms,
+           selectedProportion=input$measurementSelectedProportion)
+    })
+  
+  # TAB - RCS ####
+  # ui - user interface to select a numerical variable to associate with the presence of symptom ###
   output$rcsUI= renderUI({
     print("UI for rcs variable selection")
     selectInput(inputId="rcsIDVar",
                 label="Numerical variable", 
-                choices=dataVariableNames(),#[-c(input$selectedSymptoms)],
+                choices=dataVariableNames(),
                 selected=NULL, multiple=FALSE)   
   })
   
-  ########## user interface to select which measurments to cluster
+  # ui - user interface to select which measurments to cluster ###
   output$rcsUI2 = renderUI({
     #select the measurement
     selectInput(inputId="measurementSelectedrcs",
@@ -320,72 +256,80 @@ NumRows <- function(){ceiling(length(input$selectedSymptoms)/3)*300  }
                 choices=Measurement(), selected=NULL)
   })
   
-
-  
+  # plot - RCS plot ###
   output$plotRCS=renderPlot({
     plotRCS(data.all=dataExtended(),
-                         data.yn=dataFiltered.yn(),
-                         measurement=Measurement(),
-                         selectedSymptoms=input$selectedSymptoms,
-                         measurementSelectedrcs=input$measurementSelectedrcs,
-                         rcsIDVar=input$rcsIDVar)   
+            data.yn=dataFiltered.yn(),
+            measurement=Measurement(),
+            selectedSymptoms=input$selectedSymptoms,
+            measurementSelectedrcs=input$measurementSelectedrcs,
+            rcsIDVar=input$rcsIDVar)   
   }, height=NumRows)
   
-################ association of variables with the outcome using logistic regression with Firth correction
-
-########## user interface to select which measurments to cluster
-output$logistfUI = renderUI({
-  #select the measurement
-  selectInput(inputId="measurementSelectedlogistf",
-              label="Select the measurement (time)", 
-              choices=Measurement(), selected=Measurement()[1])
+  ################ association of variables with the outcome using logistic regression with Firth correction
+  
+  # TAB - Logistf ####
+  # ui - user interface to select which measurments to cluster ###
+  output$logistfUI = renderUI({
+    #select the measurement
+    selectInput(inputId="measurementSelectedlogistf",
+                label="Select the measurement (time)", 
+                choices=Measurement(), selected=Measurement()[1])
   })
-
   
-########## user interface to select a numerical variable to associate with the presence of symptom
-output$logistfUI2= renderUI({
   
-  print("UI for logistf variable selection")
-  selectInput(inputId="logistfIDVar",
-              label="Select a numeric variable to associate with presence of symptoms:", 
-              choices=dataVariableNames())
+  # ui - user interface to select a numerical variable to associate with the presence of symptom ###
+  output$logistfUI2= renderUI({
+    print("UI for logistf variable selection")
+    selectInput(inputId="logistfIDVar",
+                label="Select a numeric variable to associate with presence of symptoms:", 
+                choices=dataVariableNames())
   })
-
-
-output$plotLogistf <- renderPlot({
-  plotLogistf(data=dataExtended(),
-              data.yn=dataFiltered.yn(),
-              measurement=Measurement(),
-              measurementSelectedlogistf=input$measurementSelectedlogistf,
-              logistfIDVar=input$logistfIDVar,
-              selectedSymptoms=input$selectedSymptoms,
-              numSymptoms=length(input$selectedSymptoms))
   
-  
+  # plot - logistf ###
+  output$plotLogistf <- renderPlot({
+    plotLogistf(data=dataExtended(),
+                data.yn=dataFiltered.yn(),
+                measurement=Measurement(),
+                measurementSelectedlogistf=input$measurementSelectedlogistf,
+                logistfIDVar=input$logistfIDVar,
+                selectedSymptoms=input$selectedSymptoms,
+                numSymptoms=length(input$selectedSymptoms))
   })
-
-### TEMP
-
-# build the second graph
-output$tablePyramid <- renderTable ({
-  print("table pyramid")
-  ### TODO: adapt the height of the figure based on the number of symptoms: does not work now>>>> 
-  # browser()
-  #  num.symptoms=length(unlist(levels(data()[,"variable"])))
+  
+  # TAB - Selected transformed data ####
+  # table - list the subseted data in an output slot ###
+  output$data <- renderTable({
+    data <- dataExtended()
+    data$Date <- as.character(as.Date(data$Date, origin="1899-12-30"),format="%d.%m.%Y")
+    return(data)
+    # NOTE: if we want to render the table of data, we have to convert the dates into 
+    # characters, since renderTable seems to use xtable, which seems to not handle
+    # dates very well (http://stackoverflow.com/questions/8652674/r-xtable-and-dates)
+  })
+  
+  # TAB - Debug ####
+  # table - debuging information ###
+  output$debug <- renderTable(dataFiltered.yn())
   
   
-  my.table.list=TablePropWithSymptoms(data=dataFiltered(),
-                                      grouping=input$groupingVar,
-                                      measurements="Measurement",
-                                      symptomsNames=input$selectedSymptoms)
+  ### TEMP CODE ####
+  
+  # build the second graph
+  output$tablePyramid <- renderTable ({
+    print("table pyramid")
+    ### TODO: adapt the height of the figure based on the number of symptoms: does not work now>>>> 
+    # browser()
+    #  num.symptoms=length(unlist(levels(data()[,"variable"])))
+    my.table.list=TablePropWithSymptoms(data=dataFiltered(),
+                                        grouping=input$groupingVar,
+                                        measurements="Measurement",
+                                        symptomsNames=input$selectedSymptoms)
+  
+    return(my.table.list[[1]])
+  })
   
   
-  
-  return(my.table.list[[1]])
-  
-})
-
-
 })
 
 
