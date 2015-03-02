593ba351bbdefcd74c121dd2b6a4730a4a020040
Crt Ahlin <crt.ahlin@gmail.com>
2015-03-02 16:12:15 +0100

    Remove require(shiny) - import from NAMESPACE

--- medplot/R/RunApps.R
+++ medplot/R/RunApps.R
@@ -2,7 +2,5 @@
 #' 
 #' @description Function call loads the shiny app in located the default installation folder.
 medplotOnline <- function(){
-  require("shiny")
   runApp(file.path(path.package("medplot"), "shinyapp_symptoms2" ))
-
 }
