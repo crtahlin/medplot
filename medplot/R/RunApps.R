a184249efc40879087786eb999f4c502590fdfe2
Crt Ahlin <crt.ahlin@gmail.com>
2015-03-03 11:23:03 +0100

    Create NAMESPACE via roxygen2

--- medplot/R/RunApps.R
+++ medplot/R/RunApps.R
@@ -1,6 +1,10 @@
 #' @title Function to load the medplot plotSymptoms shiny web app
 #' 
 #' @description Function call loads the shiny app in located the default installation folder.
+#' 
+#' @import xtable
+#' 
+#' @export
 medplotOnline <- function(){
   runApp(file.path(path.package("medplot"), "shinyapp_symptoms2" ))
 }
