1377d0ac41d10358b31ad6ce5e8fb7184acd9308
Crt Ahlin <crt.ahlin@gmail.com>
2015-03-03 12:35:16 +0100

    Add help.

--- medplot/R/PlotSymptomsLinearModel.R
+++ medplot/R/PlotSymptomsLinearModel.R
@@ -1,36 +1,32 @@
+#' @title Return a list of result for linear modeling
+#' 
+#' @description Returns a list containing results of linear modeling. 
+#' The list contains the printable (for GUI output) and raw results (for ploting).
+#' 
+#' @param data The data.
+#' @param measurementVar Which variable represent the measurement occasion.
+#' @param selectedMeasurement Which measurement occasion was selected.
+#' @param Which covariates are to be used.
+#' @param Which outcome variables were selected.
+#' 
+#' @export
 tableLinear <- function(data,
-                           measurementVar,
-                           selectedMeasurement,
-                           covariate,
-                           selectedSymptoms#,
-                           #thresholdValue
-                           ) { 
-
+                        measurementVar,
+                        selectedMeasurement,
+                        covariate,
+                        selectedSymptoms) { 
+  
   # subset the data, using only the selected evaluation
   data <- data[data[,measurementVar]==selectedMeasurement, ]
-  
-#   # binarize the data
-#   data[, selectedSymptoms] <- ifelse(data[, selectedSymptoms]>thresholdValue, 1, 0)
   
   table <- data.frame("Variable"=selectedSymptoms) # table of printable results - Fixed effect
   table2 <- data.frame("Variable"=selectedSymptoms) # table of raw results
   table3 <- data.frame("Variable"=selectedSymptoms) # table of printable results - Intercept
   
-#   # check if covariate is binary and generate text which levels we are comparing
-#   if (determineTypeofVariable(data[,covariate])[["nLevels"]]=="binary") { # binary var
-#     levels <- levels(as.factor(data[,covariate])) # levels of the covariate
-#     oddsFor <- paste(levels[2],"vs",levels[1]) # text describing which variables were compared
-#   }
-#   
-#   if (determineTypeofVariable(data[,covariate])[["nLevels"]]=="multilevel" &  # numerical var w multi levels
-#         ( determineTypeofVariable(data[,covariate])[["type"]]=="integer") 
-#       | determineTypeofVariable(data[,covariate])[["type"]]=="numeric") { 
-#     #levels <- levels(as.factor(data[,covariate])) # levels of the covariate
-#     oddsFor <- paste("unit difference in", covariate) # text describing which variables were compared
-#   }
-#   
   for (symptom in selectedSymptoms) {
     model <- glm(data[,symptom] ~ data[,covariate], family=gaussian())
+    
+    # printable and raw table of coefficients
     table[table["Variable"]==symptom, "Beta (slope)"] <- 
       format(model$coef[2], digits=2)
     table[table["Variable"]==symptom, "95% conf. interval"] <- 
@@ -52,19 +48,9 @@ tableLinear <- function(data,
             format((confint(model)[1,2]), digits=2))
     table3[table["Variable"]==symptom, "P value"] <-
       format(summary(model)$coefficients[1,"Pr(>|t|)"],digits=2)
-  }
+    }
+  
   return(list(printableResultsTable=table,
               rawResultsTable=table2,
               printableInterceptTable=table3))
-  
-}
-
-
-
-# head(mtcars)
-# model <- glm(mtcars$mpg~mtcars$hp, family=gaussian)
-# str(model)
-# summary(model)
-# confint(model)
-# coef(model)
-# model$coef[2]
+  }
