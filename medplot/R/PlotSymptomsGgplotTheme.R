a543bcfce7a98bd9cf38f5dc96680b75d763edc3
Crt Ahlin <crt.ahlin@gmail.com>
2015-03-03 12:09:28 +0100

    Add help.

--- medplot/R/PlotSymptomsGgplotTheme.R
+++ medplot/R/PlotSymptomsGgplotTheme.R
@@ -1,6 +1,9 @@
-#define a ggplot2 theme
+#' @title Define a ggplot2 theme used in app
+#' 
+#' @export
 myTheme <- function () {
   myTheme <- theme_bw() + theme(
     text = element_text(size=18)
-  )
-  return(myTheme)}
+    )
+  return(myTheme)
+  }
